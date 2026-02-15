547b9873a9779e1405616e717f078fb3dc70a422
ramez <ramez.elzoz74@gmail.com>
2026-02-15 20:35:50 +0200

    adding the functionality of uploading files

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -23,7 +23,7 @@ import FileUploader from "./FileUploader";
 import { signOutUser } from "@/lib/actions/user.actions";
 
 interface Props {
-  ownerId: string;
+  $id: string;
   accountId: string;
   fullName: string;
   avatar: string;
@@ -31,7 +31,7 @@ interface Props {
 }
 
 const MobileNavigation = ({
-  ownerId,
+  $id: ownerId,
   accountId,
   fullName,
   avatar,
@@ -106,7 +106,7 @@ const MobileNavigation = ({
           <Separator className="my-5 bg-light-200/20" />
 
           <div className="flex flex-col justify-between gap-5">
-            <FileUploader />
+            <FileUploader ownerId={ownerId} accountId={accountId} />
             <Button
               type="submit"
               className="mobile-sign-out-button"
