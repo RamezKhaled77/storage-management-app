a94172f720b1d8c35a47069b668c713eac558c71
ramez <ramez.elzoz74@gmail.com>
2026-02-15 15:15:18 +0200

    finishing the sidebar with responsive

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,11 +1,12 @@
 "use server";
 
 import { ID, Query } from "node-appwrite";
-import { createAdminClient } from "../appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
 import path from "path";
 import { cookies } from "next/headers";
+import { avatarPlaceholderUrl } from "@/constants";
 
 const getUserByEmail = async (email: string) => {
   const { databases } = await createAdminClient();
@@ -57,8 +58,7 @@ export const createAccount = async ({
       {
         fullName,
         email,
-        avatar:
-          "https://static.vecteezy.com/system/resources/thumbnails/067/451/107/small/avatar-default-user-profile-icon-social-media-profile-picture-symbol-person-icon-gender-neutral-outline-round-shape-sign-user-account-dp-symbol-isolated-on-white-background-illustration-vector.jpg",
+        avatar: avatarPlaceholderUrl,
         accountId,
       },
     );
@@ -83,4 +83,20 @@ export const verifySecert = async (accountId: string, password: string) => {
   } catch (error) {
     handleError(error, "Failed to verify OTP");
   }
+};
+
+export const getCurrentUser = async () => {
+  const { databases, account } = await createSessionClient();
+
+  const result = await account.get();
+
+  const user = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersTableId,
+    [Query.equal("accountId", result.$id)],
+  );
+
+  if (!user.total) return null;
+
+  return parseStringify(user.documents[0]);
 };
